afcdb33db69934e5cf933c06c3ffae46e2e8bd6c
DongKyuK12 <jkdk981024@gmail.com>
2024-08-24 06:37:02 +0900

    첫커밋

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-106pxr font-bold text-blue-600">Hello Vite + React + TypeScript + Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,47 @@
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+
+type AccType = Record<string, string>;
+
+const range = (start: number, end: number): number[] => {
+  const array: number[] = [];
+  for (let i = start; i <= end; i += 1) {
+    array.push(i);
+  }
+  return array;
+};
+
+const pxToRem = (px: number, base = 16) => `${px / base}rem`;
+
+const config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    spacing: {
+      ...range(0, 1600).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px);
+        return acc;
+      }, {}),
+    },
+    fontSize: {
+      ...range(0, 200).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px);
+        return acc;
+      }, {}),
+    },
+    extend: {
+      colors: {},
+      keyframes: {},
+      animation: {},
+      screens: {},
+    },
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      const newUtilities = {};
+
+      addUtilities(newUtilities);
+    }),
+  ],
+} satisfies Config;
+
+export default config;
