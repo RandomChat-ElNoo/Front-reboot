9fe59512985a253d9568c7fd515d82ad7da2a411
DongKyuK12 <jkdk981024@gmail.com>
2024-08-24 07:33:30 +0900

    두번째 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-screen bg-gray-500">
       <h1 className="text-106pxr font-bold text-blue-600">Hello Vite + React + TypeScript + Tailwind CSS!</h1>
     </div>
   );
