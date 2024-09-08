ca984a6b76ab82635b29ae1eb98d698351b051c6
el_di <eldi.basic@gmail.com>
2024-09-08 19:39:42 +0900

    tailwind 색깔 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,22 @@ const config = {
         return acc
       }, {}),
     },
+    colors: {
+      'background-mian': '#000000',
+      'button-mian': '#81818D',
+      'button-main-hover': '#9E9EAC',
+      'background-sidebar': '#000000',
+      'button-sidebar': '#3A3A41',
+      'button-sidebar-hover': '#6C6C73',
+      'button-sidebar-text': '#FFFFFF',
+      'text-box-text': '#C7C8D1',
+      'alert-red': '#F23F42',
+      'chat-box': '#595C6C',
+      'chat-box-me': '#575B78',
+      'button-green': '#2CDB75',
+      'button-blue': '#5865F2',
+      'button-gray': '#909090',
+    },
     extend: {
       colors: {},
       keyframes: {},
