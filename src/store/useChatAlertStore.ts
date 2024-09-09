464730425992f79a40126effd08bb1144d93806b
DongKyuK12 <jkdk981024@gmail.com>
2024-09-09 19:09:31 +0900

    짜잘한 수정

--- src/store/useChatAlertStore.ts
+++ src/store/useChatAlertStore.ts
@@ -1,12 +1,12 @@
 import create from 'zustand'
 
-interface BearStore {
+interface ChatAlertStore {
   randomChatAlert: number
   setRandomChatAlert: (value: number) => void
   groupChatAlert: number
   setGroupChatAlert: (value: number) => void
 }
-const useChatAlertStore = create<BearStore>((set) => ({
+const useChatAlertStore = create<ChatAlertStore>((set) => ({
   randomChatAlert: 0,
   setRandomChatAlert: (value: number) => set({ randomChatAlert: value }),
   groupChatAlert: 0,
