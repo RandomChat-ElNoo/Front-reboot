9f5127bff4c98440a8808cb21be9f8184f514242
DongKyuK12 <jkdk981024@gmail.com>
2024-09-09 13:02:55 +0900

    feat: 옵션 컴포넌트 추가, useOptionStore로 상태 관리

---
+++ src/components/sideBar/Options.tsx
@@ -0,0 +1,98 @@
+import { CloseOutlined, UnorderedListOutlined } from '@ant-design/icons'
+import { Button, ConfigProvider, Input, Switch } from 'antd'
+import useOptionStore from '../../store/useOptionStore'
+import { useState } from 'react'
+
+/**
+ * 사이드바 하단에 있는 옵션창
+ * @todo 나중에 닫는 함수 넣어줘야함
+ */
+
+export default function Options() {
+  const {
+    avatar,
+    setAvatar,
+    isRandomChatImgPreview,
+    setIsRandomChatImgPreview,
+    isGroupChatImgPreview,
+    setIsGroupChatImgPreview,
+  } = useOptionStore()
+  const [avatarInputValue, setAvatarInputValue] = useState('')
+
+  const setAvatarState = () => {
+    if (avatarInputValue === '') {
+      setAvatar('기타')
+      return
+    }
+
+    setAvatar(avatarInputValue)
+  }
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#2CDB75',
+        },
+        components: {
+          Switch: {},
+        },
+      }}
+    >
+      <div className="w-full">
+        <section className="flex h-40pxr flex-row items-center justify-between bg-background-sidebar-bottom px-10pxr">
+          <UnorderedListOutlined style={{ fontSize: '28px', color: 'white' }} />
+          <p className="text-18pxr text-white">옵션</p>
+          <CloseOutlined
+            onClick={() => {}} // 나중에 옵션 닫는 함수 넣어줘야함
+            style={{ fontSize: '28px', color: 'white' }}
+          />
+        </section>
+        <section className="flex w-full flex-col gap-10pxr bg-background-sidebar px-10pxr py-20pxr">
+          <p className="text-18pxr text-white">이미지 미리보기</p>
+          <div className="flex flex-row justify-between">
+            <p className="text-18pxr text-white">전체 채팅</p>
+            <Switch
+              defaultValue={isRandomChatImgPreview}
+              onChange={() => {
+                setIsRandomChatImgPreview(!isRandomChatImgPreview)
+              }}
+              className="bg-black"
+            />
+          </div>
+          <div className="flex flex-row justify-between">
+            <p className="text-18pxr text-white">랜덤 채팅</p>
+            <Switch
+              defaultValue={isGroupChatImgPreview}
+              onChange={() => {
+                setIsGroupChatImgPreview(!isGroupChatImgPreview)
+              }}
+              className="bg-black"
+            />
+          </div>
+          <div className="flex flex-col gap-5pxr">
+            <div className="flex flex-row items-center justify-between">
+              <p className="text-18pxr text-white">아바타</p>
+              <Button
+                disabled={avatar === avatarInputValue}
+                onClick={setAvatarState}
+                className="rounded-full bg-button-green text-white"
+              >
+                적용
+              </Button>
+            </div>
+            <Input
+              className="rounded-[10px]"
+              maxLength={10}
+              onChange={(e) => {
+                setAvatarInputValue(e.target.value)
+              }}
+              onPressEnter={setAvatarState}
+              defaultValue={avatar}
+              count={{ max: 10, show: true }}
+            />
+          </div>
+        </section>
+      </div>
+    </ConfigProvider>
+  )
+}
---
+++ src/store/useOptionStore.ts
@@ -0,0 +1,36 @@
+import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+interface useOptionStoreProps {
+  avatar: string
+  setAvatar: (state: string) => void
+  isRandomChatImgPreview: boolean
+  setIsRandomChatImgPreview: (state: boolean) => void
+  isGroupChatImgPreview: boolean
+  setIsGroupChatImgPreview: (state: boolean) => void
+}
+
+const useOptionStore = create(
+  persist<useOptionStoreProps>(
+    (set) => ({
+      avatar: '기타',
+      setAvatar: (state) => {
+        set({ avatar: state })
+      },
+      isRandomChatImgPreview: false,
+      setIsRandomChatImgPreview: (state) => {
+        set({ isRandomChatImgPreview: state })
+      },
+      isGroupChatImgPreview: false,
+      setIsGroupChatImgPreview: (state) => {
+        set({ isGroupChatImgPreview: state })
+      },
+    }),
+    {
+      name: 'options',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+)
+
+export default useOptionStore
