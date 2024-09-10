d37964d6d9088a71a35fac041b68cd73c51d9207
el_di <eldi.basic@gmail.com>
2024-09-10 16:22:08 +0900

    logo와 버튼 사이 간격 30으로 수정

--- src/components/MainButtons.tsx
+++ src/components/MainButtons.tsx
@@ -12,42 +12,44 @@ export default function MainButtons() {
   const [isDonationModalOpen, setIsDonationModalOpen] = useState(false)
   return (
     <>
-      <div className="flex w-full max-w-530pxr flex-col items-center gap-20pxr">
+      <div className="flex w-full max-w-530pxr flex-col items-center gap-30pxr">
         <img className="h-350pxr w-350pxr" src={logoFull} />
-        <div className="flex w-full flex-row gap-30pxr">
+        <div className="flex w-full max-w-530pxr flex-col items-center gap-20pxr">
+          <div className="flex w-full flex-row gap-30pxr">
+            <MainButton
+              title="전체 채팅"
+              context="익명으로 유저들과 대화해요"
+              buttonType="high"
+              onClick={() => {}}
+            />
+            <MainButton
+              title="랜덤 채팅"
+              context="랜덤 유저와 대화해요"
+              buttonType="high"
+              onClick={() => {}}
+            />
+          </div>
           <MainButton
-            title="전체 채팅"
-            context="익명으로 유저들과 대화해요"
-            buttonType="high"
-            onClick={() => {}}
+            title="문의하기"
+            context="메일로 추가 기능 건의 및 문의하기"
+            buttonType="wide"
+            onClick={() => setIsContactModalOpen(true)}
           />
           <MainButton
-            title="랜덤 채팅"
-            context="랜덤 유저와 대화해요"
-            buttonType="high"
-            onClick={() => {}}
+            title="후원하기"
+            context="서버비를 지원해요"
+            buttonType="wide"
+            onClick={() => setIsDonationModalOpen(true)}
+          />
+          <ContactModal
+            isOpen={isContactModalOpen}
+            closeModal={() => setIsContactModalOpen(false)}
+          />
+          <DonationModal
+            isOpen={isDonationModalOpen}
+            closeModal={() => setIsDonationModalOpen(false)}
           />
         </div>
-        <MainButton
-          title="문의하기"
-          context="메일로 추가 기능 건의 및 문의하기"
-          buttonType="wide"
-          onClick={() => setIsContactModalOpen(true)}
-        />
-        <MainButton
-          title="후원하기"
-          context="서버비를 지원해요"
-          buttonType="wide"
-          onClick={() => setIsDonationModalOpen(true)}
-        />
-        <ContactModal
-          isOpen={isContactModalOpen}
-          closeModal={() => setIsContactModalOpen(false)}
-        />
-        <DonationModal
-          isOpen={isDonationModalOpen}
-          closeModal={() => setIsDonationModalOpen(false)}
-        />
       </div>
     </>
   )
