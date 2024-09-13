793be6c4fc17c71dfaedc24dc0b681cd53490841
DongKyuK12 <jkdk981024@gmail.com>
2024-09-13 19:11:44 +0900

    홈페이지의 메인로고 png로 변경 및 page 컴포넌트로 페이지 구조 변경

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,11 @@
+import MainButtons from './MainButtons'
+
+/** 홈 화면 */
+
+export default function Home() {
+  return (
+    <div className="flex w-full flex-row justify-center px-10pxr pt-60pxr">
+      <MainButtons />
+    </div>
+  )
+}
