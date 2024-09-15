42ead1231dadaee226286beb3c0692895abf36f7
DongKyuK12 <jkdk981024@gmail.com>
2024-09-15 15:30:58 +0900

    useState의 콜백 값 넣는 기능 zustand로 구현

--- src/store/useChatAlertStore.ts
+++ src/store/useChatAlertStore.ts
@@ -2,16 +2,26 @@ import { create } from 'zustand'
 
 interface ChatAlertStore {
   randomChatAlert: number
-  setRandomChatAlert: (value: number) => void
+  setRandomChatAlert: (value: number | ((prev: number) => number)) => void
   groupChatAlert: number
-  setGroupChatAlert: (value: number) => void
+  setGroupChatAlert: (value: number | ((prev: number) => number)) => void
 }
 
 const useChatAlertStore = create<ChatAlertStore>((set) => ({
   randomChatAlert: 0,
-  setRandomChatAlert: (value: number) => set({ randomChatAlert: value }),
+  setRandomChatAlert: (value) => {
+    set((state) => ({
+      groupChatAlert:
+        typeof value === 'function' ? value(state.groupChatAlert) : value,
+    }))
+  },
   groupChatAlert: 0,
-  setGroupChatAlert: (value: number) => set({ randomChatAlert: value }),
+  setGroupChatAlert: (value) => {
+    set((state) => ({
+      groupChatAlert:
+        typeof value === 'function' ? value(state.groupChatAlert) : value,
+    }))
+  },
 }))
 
 export default useChatAlertStore
