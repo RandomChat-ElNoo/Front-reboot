7e1e842fb40b1147cacde95a17e2922b9f7f7054
DongKyuK12 <jkdk981024@gmail.com>
2024-09-15 15:33:54 +0900

    페이지 언마운트 되지않게 css로 페이지 이동 구현

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react'
 import usePageStore from '../store/usePageStore'
+import GroupChatPage from './chat/GroupChatPage'
 import Home from './home/Home'
+import useChatAlertStore from '../store/useChatAlertStore'
 
 /**
  * 본문 부분에 랜더링 되는 부분
@@ -7,13 +10,28 @@ import Home from './home/Home'
  */
 export default function Pages() {
   const { page } = usePageStore()
+  const { setGroupChatAlert, setRandomChatAlert } = useChatAlertStore()
   const homePageShown = page === 0 ? '' : 'hidden'
+  const groupChatPageShown = page === 1 ? '' : 'hidden'
+  const randomChatPageShown = page === 2 ? '' : 'hidden'
+  useEffect(() => {
+    if (page === 1) {
+      setGroupChatAlert(0)
+    }
+    if (page === 2) {
+      setRandomChatAlert(0)
+    }
+  }, [page])
+
   return (
-    <div className="h-full w-full">
+    <div className="h-[calc(100vh-50px)] w-full">
       <div className={`${homePageShown} h-full w-full`}>
         <Home />
       </div>
-      {page === 1 ? '' : page === 2 ? '' : ''}
+      <div className={`${groupChatPageShown} h-full w-full`}>
+        <GroupChatPage />
+      </div>
+      <div className={`${randomChatPageShown} h-full w-full`}></div>
     </div>
   )
 }
