4856e1c6bc5707e39320d898ee7c4d61d648bb7a
DongKyuK12 <jkdk981024@gmail.com>
2024-09-22 22:18:46 +0900

    webp로 바뀐 것들 수정

--- src/constant/emoji.ts
+++ src/constant/emoji.ts
@@ -1,8 +1,8 @@
 export const EMOJI_ARRAY = [
-  { name: 'hi', src: '/imgs/emojis/hi.png' },
-  { name: 'bye', src: '/imgs/emojis/bye.png' },
-  { name: 'lol', src: '/imgs/emojis/lol.png' },
-  { name: 'angry', src: '/imgs/emojis/angry.png' },
-  { name: 'dizzy', src: '/imgs/emojis/dizzy.png' },
-  { name: 'freak', src: '/imgs/emojis/freak.png' },
+  { name: 'hi', src: '/imgs/emojis/hi.webp' },
+  { name: 'bye', src: '/imgs/emojis/bye.webp' },
+  { name: 'lol', src: '/imgs/emojis/lol.webp' },
+  { name: 'angry', src: '/imgs/emojis/angry.webp' },
+  { name: 'dizzy', src: '/imgs/emojis/dizzy.webp' },
+  { name: 'freak', src: '/imgs/emojis/freak.webp' },
 ]
