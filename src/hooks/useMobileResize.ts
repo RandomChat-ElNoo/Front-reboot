7ee141be74fe2478210dbd94c0cac719a862d876
DongKyuK12 <jkdk981024@gmail.com>
2024-09-23 17:27:14 +0900

    추가 테스트

--- src/hooks/useMobileResize.ts
+++ src/hooks/useMobileResize.ts
@@ -5,20 +5,31 @@ export default function useMobileResize() {
 
   // 화면의 높이를 동적으로 조정하는 함수
   const adjustHeight = () => {
-    setHeight(window.innerHeight)
-    console.log(window.innerHeight)
+    if (window.visualViewport) {
+      setHeight(window.visualViewport.height)
+      console.log(window.visualViewport.height)
+    } else {
+      setHeight(window.innerHeight)
+      console.log(window.innerHeight)
+    }
   }
 
   useEffect(() => {
-    // 컴포넌트가 마운트될 때와 리사이즈 이벤트가 발생할 때 높이 조정
-    window.addEventListener('resize', adjustHeight)
-
+    if (window.visualViewport) {
+      window.visualViewport.addEventListener('resize', adjustHeight)
+    } else {
+      window.addEventListener('resize', adjustHeight)
+    }
     // 초기 높이 설정
     adjustHeight()
 
     // 컴포넌트가 언마운트될 때 이벤트 제거
     return () => {
-      window.removeEventListener('resize', adjustHeight)
+      if (window.visualViewport) {
+        window.visualViewport.removeEventListener('resize', adjustHeight)
+      } else {
+        window.removeEventListener('resize', adjustHeight)
+      }
     }
   }, [])
 
